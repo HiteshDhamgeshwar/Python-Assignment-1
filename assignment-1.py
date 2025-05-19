96f50d7d8f27a700bb9166a770747237ffac0829
Hitesh Kumar <821kumarhitesh@gmail.com>
2025-05-19 16:03:57 +0530

    Python-Assignment-1 py file uploaded

---
+++ assignment-1.py
@@ -0,0 +1,35 @@
+#Task1:
+# input
+
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+#output for addition
+Addition= int(num1) + int(num2)
+print(Addition)
+
+#output for subtraction
+Subtraction=int(num1) - int(num2)
+print(Subtraction)
+
+#output for multiplication
+Multiplication=int(num1) * int(num2)
+print(Multiplication)
+
+#output for division
+Division=float(num1) / float(num2)
+print(Division)
+
+
+#Task2:
+# user's first name
+first_name = input("Enter your first name: ")
+
+# user's last name
+last_name = input("Enter your last name: ")
+
+# Concatenate first and last name
+full_name = first_name + " " + last_name
+
+# Print personalized greeting message
+print("Hello, " + full_name + "! Welcome to the python program.")
